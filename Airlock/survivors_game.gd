51aa9e5b9ab8464a830e96793d3c3257bec9aca6
Noah77A <hartglassn@gmail.com>
2026-09-09 19:28:16 -0400

    Fixed the menu fade issue by creating menus own scene. Also changed the main startup scene to Menu so we dont get any accidental gameplay before we start
    Also added the spritesheet for the intro sequence

--- Airlock/survivors_game.gd
+++ Airlock/survivors_game.gd
@@ -1,6 +1,12 @@
 extends Node2D
 
 
+func _ready():
+	%Timer.start()
+	%OxygenMeter.wait_time = GameManager.oxygen + 60
+	%OxygenBar.max_value = %OxygenMeter.wait_time
+	%OxygenMeter.start()
+	
 
 func spawn_mob():
 	var new_mob = preload("res://mob.tscn").instantiate()
@@ -9,13 +15,13 @@ func spawn_mob():
 	add_child(new_mob)
 	%OxygenBar.value = %OxygenMeter.time_left
 func spawn_tree(): 
-	var new_tree = preload("res://pine_tree.tscn").instantiate()
+	var new_tree = preload("res://trees/pine_tree.tscn").instantiate()
 	%PathFollow2D.progress_ratio = randf()
 	new_tree.global_position = %PathFollow2D.global_position
 	add_child(new_tree)
 
 func spawn_red_tree(): 
-	var new_tree = preload("res://red_pine_tree.tscn").instantiate()
+	var new_tree = preload("res://trees/red_pine_tree.tscn").instantiate()
 	%PathFollow2D.progress_ratio = randf()
 	new_tree.global_position = %PathFollow2D.global_position
 	add_child(new_tree)
@@ -29,36 +35,6 @@ func _on_timer_timeout():
 func _on_player_health_depleted():
 	%"Game Over".visible = true
 	get_tree().paused = true
-
-
-func _on_start_pressed():
-	%Timer.start()
-	%MainMenu.queue_free()
-	%OxygenMeter.wait_time = GameManager.oxygen + 60
-	%OxygenBar.max_value = %OxygenMeter.wait_time
-	%OxygenMeter.start()
-	
-
-
-func _on_start_timer_timeout() -> void:
-	%WhiteBackground.visible = false
-	%MainMenu.visible = true
-
-
-@export var credits_scene: PackedScene
-func _on_credits_pressed() -> void:
-	if credits_scene:
-		var creditsScreen = credits_scene.instantiate()
-		add_child(creditsScreen)
-
-	
-	
-
-@export var version_scene: PackedScene
-func _on_version_pressed() -> void:
-	if version_scene:
-		var versionScreen = version_scene.instantiate()
-		add_child(versionScreen)
 
 
 func _on_oxygen_meter_timeout() -> void:
