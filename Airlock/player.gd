ccd16a3020de0df5f6acc4cc531e3d8480b4018f
Eyufoi <aiddelco@gmail.com>
2026-09-09 17:19:16 -0400

    added room loading and room switching and scaled everything to fit with the room scale

--- Airlock/player.gd
+++ Airlock/player.gd
@@ -7,7 +7,7 @@ func _physics_process(delta):
 	
 	
 	var direction = Input.get_vector("move_left","move_right","move_up","move_down")
-	velocity = direction * 600
+	velocity = direction * 60
 	move_and_slide()
 	
 	if velocity.length() > 0.0:
--- Airlock/survivors_game.gd
+++ Airlock/survivors_game.gd
@@ -21,9 +21,10 @@ func spawn_red_tree():
 	add_child(new_tree)
 	
 func _on_timer_timeout():
-	spawn_mob()
-	spawn_tree()
-	spawn_red_tree()
+	return
+	#spawn_mob()
+	#spawn_tree()
+	#spawn_red_tree()
 
 
 func _on_player_health_depleted():
