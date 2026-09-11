6bf5c9cf8f226a6b4adf987bc93d5a05f4df90f1
Noah77A <hartglassn@gmail.com>
2026-09-11 13:38:16 -0400

    Updated the intro sequence and menu. game now starts in the intro sequence and you can click through it
    Also got rid of the extra version notes. Lol

--- Airlock/assets/animated_sprite_2d.gd
+++ Airlock/assets/animated_sprite_2d.gd
@@ -9,11 +9,11 @@ func increase_frame():
 	if (frame < 6):
 		frame += 1
 	else:
-		queue_free()	
+		get_tree().change_scene_to_file("res://survivors_game.tscn")
 	
 		
 
 
 func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	if (event.is_action_pressed("click")):
+	if (event.is_action_pressed("shoot")):
 		increase_frame()# Replace with function body.
