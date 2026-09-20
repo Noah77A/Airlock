f86103e77a9c9383085dce3d6508536d1b86ab4d
Noah77A <hartglassn@gmail.com>
2026-09-20 12:26:53 -0400

    Added a temporary solution to how the game just ends when you die.
    Waits 10 seconds on the game over screen, then goes back to the menu. I'll prob add a menu button later

--- Airlock/survivors_game.gd
+++ Airlock/survivors_game.gd
@@ -26,3 +26,8 @@ func spawn_red_tree():
 func _on_player_health_depleted():
 	%"Game Over".visible = true
 	get_tree().paused = true
+	await get_tree().create_timer(5.0).timeout
+	get_tree().paused = false;
+	get_tree().change_scene_to_file("res://menu.tscn")
+	
+	
