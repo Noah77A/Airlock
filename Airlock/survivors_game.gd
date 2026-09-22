9b4b918e08d28084873fc536b0b72042135a8a80
Noah77A <hartglassn@gmail.com>
2026-09-22 10:50:17 -0400

    Added buttons at the game over screen to not brick the game anymore
    Return to menu and a quit button

--- Airlock/survivors_game.gd
+++ Airlock/survivors_game.gd
@@ -26,8 +26,16 @@ func spawn_red_tree():
 func _on_player_health_depleted():
 	%"Game Over".visible = true
 	get_tree().paused = true
-	await get_tree().create_timer(5.0).timeout
-	get_tree().paused = false;
+	
+	
+	
+	
+
+
+func _on_return_to_menu_pressed() -> void:
+	get_tree().paused = false
 	get_tree().change_scene_to_file("res://menu.tscn")
-	
-	
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit(0)
