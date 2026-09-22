c14aee8b7a065dfa3221e9e69da171b8651ee2e9
Eyufoi <aiddelco@gmail.com>
2026-09-22 11:00:11 -0400

    added some music and sound effects to the aliens

--- Airlock/characters/slime/slime.gd
+++ Airlock/characters/slime/slime.gd
@@ -1,10 +1,19 @@
 extends Node2D
 
+@onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
+
 
 func play_walk():
 	%AlienAnimation.walk()
 
-
+func _process(delta: float) -> void:
+	var len = audio_stream_player_2d.stream.get_length()
+	if (!audio_stream_player_2d.playing && randf() > 0.99):
+		audio_stream_player_2d.play(randf()*len)
+	else: if (audio_stream_player_2d.get_playback_position() > 2.0):
+		audio_stream_player_2d.stop()
+		
+	
 
 func play_hurt():
 	modulate = Color(0.651, 0.0, 0.0, 1.0)
