988c6ad9d9060fdaea01242d93ea6b19b4a77b01
Noah77A <hartglassn@gmail.com>
2026-09-26 16:33:57 -0400

    Updated collisions and sound effects

--- Airlock/characters/slime/slime.gd
+++ Airlock/characters/slime/slime.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 @onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
 var baseColor : Color = Color(1.0, 1.0, 1.0, 1.0)
-
+var sound
 func _ready():  
 	if(GameManager.difficulty == 1):
 		baseColor =  Color(1.0, 1.0, 1.0, 1.0)
@@ -15,17 +15,18 @@ func _ready():
 	else: if (GameManager.difficulty == 5):
 		baseColor = Color("5b8c36")
 	modulate = baseColor
+	sound = randf() < .10
+	
 func play_walk():
 	%AlienAnimation.walk()
 
-func _process(delta: float) -> void:
-	var len = audio_stream_player_2d.stream.get_length()
-	if (!audio_stream_player_2d.playing && randf() > 0.99):
-		audio_stream_player_2d.play(randf()*len)
-	else: if (audio_stream_player_2d.get_playback_position() > 2.0):
-		audio_stream_player_2d.stop()
-		
-	
+func _process(delta: float) -> void:	
+	if (sound):
+		var len = audio_stream_player_2d.stream.get_length()
+		if (!audio_stream_player_2d.playing && randf() > 0.99):
+			audio_stream_player_2d.play(randf()*len)
+		else: if (audio_stream_player_2d.get_playback_position() > 2.0):
+			audio_stream_player_2d.stop()
 
 func play_hurt():
 	modulate = Color(0.651, 0.0, 0.0, 1.0)
